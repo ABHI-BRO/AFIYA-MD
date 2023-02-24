5b98e254ee636c905dda676d566f0c5d811be859
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-02-24 15:09:29 +0530

    tets

--- bot.js
+++ bot.js
@@ -4,43 +4,52 @@ const {
   makeInMemoryStore,
   useMultiFileAuthState,
 } = require("@adiwajshing/baileys");
-const singleToMulti = require("./lib/singleToMulti");
 const fs = require("fs");
 const { serialize } = require("./lib/serialize");
 const { Message, Image, Sticker } = require("./lib/Base");
 const pino = require("pino");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
 const path = require("path");
+const Config = require('./config');
 const events = require("./lib/event");
 const got = require("got");
 const config = require("./config");
 const { PluginDB } = require("./lib/database/plugins");
 const Greetings = require("./lib/Greetings");
-const { MakeSession } = require("./lib/session");
-const { async } = require("q");
-const { decodeJid } = require("./lib");
 const store = makeInMemoryStore({
   logger: pino().child({ level: "silent", stream: "store" }),
 });
-async function Singmulti() {
-  if (!fs.existsSync(__dirname + "/session.json"))
-    await MakeSession(config.SESSION_ID, __dirname + "/session.json");
-  const { state } = await useMultiFileAuthState(__dirname + "/session");
-  await singleToMulti("session.json", __dirname + "/session", state);
-}
-Singmulti()
-require("events").EventEmitter.defaultMaxListeners = 0;
 
-fs.readdirSync(__dirname + "/lib/database/").forEach((plugin) => {
+require("events").EventEmitter.defaultMaxListeners = 500;
+
+
+fs.readdirSync("./lib/database/").forEach((plugin) => {
   if (path.extname(plugin).toLowerCase() == ".js") {
-    require(__dirname + "/lib/database/" + plugin);
+    require("./lib/database/" + plugin);
   }
 });
-async function Xasena() {
-  const { state, saveCreds } = await useMultiFileAuthState(
-    __dirname + "/session"
-  );
+
+const aes256 = require('aes256');
+let plaintext = Config.SESSION_ID.replaceAll("inrl~", "");
+let key = 'k!t';
+let decryptedPlainText = aes256.decrypt(key, plaintext);
+  async function md(){
+   let {body} = await got(`https://inrl-web.vercel.app/api/session?id=${decryptedPlainText}`)
+  let result = JSON.parse(body).result[0].data;
+fs.writeFileSync("./lib/auth_info_baileys/creds.json" , result);
+   }
+  md();
+
+async function ToxicLeo() {
   console.log("Syncing Database");
   await config.DATABASE.sync();
+
+const { state, saveCreds } = await useMultiFileAuthState(
+    "./lib/auth_info_baileys/",
+    pino({ level: "silent" })
+  )
   let conn = makeWASocket({
     logger: pino({ level: "silent" }),
     auth: state,
@@ -51,77 +60,76 @@ async function Xasena() {
     shouldSyncHistoryMessage: false,
     downloadHistory: false,
     syncFullHistory: false,
-    getMessage: async (key) =>
-      (store.loadMessage(key.id) || {}).message || {
-        conversation: null,
-      },
   });
+    
   store.bind(conn.ev);
+  //store.readFromFile("./database/store.json");
   setInterval(() => {
     store.writeToFile("./database/store.json");
-  }, 30 * 1000);
+  }, 30 * 60 * 1000);
 
-  conn.ev.on("creds.update", saveCreds);
-  conn.ev.on("contacts.update", (update) => {
-    for (let contact of update) {
-      let id = decodeJid(contact.id);
-      if (store && store.contacts)
-        store.contacts[id] = { id, name: contact.notify };
-    }
-  });
+ conn.ev.on("creds.update", saveCreds);
+
   conn.ev.on("connection.update", async (s) => {
     const { connection, lastDisconnect } = s;
     if (connection === "connecting") {
-      console.log("X-Asena");
-      console.log("ℹ️ Connecting to WhatsApp... Please Wait.");
+      console.log("Toxic Leo MD");
+      console.log(" ♻ Toxic leo Connecting to WhatsApp... Please Wait. ");
     }
+
+    if (
+      connection === "close" &&
+      lastDisconnect &&
+      lastDisconnect.error &&
+      lastDisconnect.error.output.statusCode != 401
+    ) {
+      console.log(lastDisconnect.error.output.payload);
+      Xasena();
+    }
+
     if (connection === "open") {
-      console.log("✅ Login Successful!");
-      console.log("⬇️ Installing External Plugins...");
+      console.log(" Login Successful! ✓ ");
+      console.log(" ❗Installing External Plugins... ");
 
       let plugins = await PluginDB.findAll();
       plugins.map(async (plugin) => {
-        if (!fs.existsSync("./plugins/" + plugin.dataValues.name + ".js")) {
+        if (!fs.existsSync("./plugin/" + plugin.dataValues.name + ".js")) {
           console.log(plugin.dataValues.name);
           var response = await got(plugin.dataValues.url);
           if (response.statusCode == 200) {
             fs.writeFileSync(
-              "./plugins/" + plugin.dataValues.name + ".js",
+              "./plugin/" + plugin.dataValues.name + ".js",
               response.body
             );
-            require(__dirname + "/plugins/" + plugin.dataValues.name + ".js");
+            require("./plugin/" + plugin.dataValues.name + ".js");
           }
         }
       });
 
-      console.log("⬇️  Installing Plugins...");
+      console.log(" Installing Plugins... ");
 
-      fs.readdirSync(__dirname + "/plugins").forEach((plugin) => {
+      fs.readdirSync("./plugin").forEach((plugin) => {
         if (path.extname(plugin).toLowerCase() == ".js") {
-          require(__dirname + "/plugins/" + plugin);
+          require("./plugin/" + plugin);
         }
       });
-      console.log("✅ Plugins Installed!");
-      let str = `\`\`\`afiya connected \nversion : ${
-        require(__dirname + "/package.json").version
-      }\nTotal Plugins : ${events.commands.length}\nWorktype: ${
+      console.log(" Plugins Installed! ");
+      let str = `\`\`\` *ʟᴇᴏ ᴍᴅ sᴛᴀʀᴛᴇᴅ* \nversion : ${
+        require("./package.json").version
+      }\nTotal Plugins : ${events.commands.length}\nMode: ${
         config.WORK_TYPE
       }\`\`\``;
       conn.sendMessage(conn.user.id, { text: str });
-
       try {
         conn.ev.on("group-participants.update", async (data) => {
           Greetings(data, conn);
         });
         conn.ev.on("messages.upsert", async (m) => {
           if (m.type !== "notify") return;
-          const ms = m.messages[0];
-          let msg = await serialize(JSON.parse(JSON.stringify(ms)), conn,store);
+          let ms = m.messages[0];
+          let msg = await serialize(JSON.parse(JSON.stringify(ms)), conn);
           if (!msg.message) return;
-          if (msg.body[1] && msg.body[1] == " ")
-            msg.body = msg.body[0] + msg.body.slice(2);
           let text_msg = msg.body;
-          msg.store = store;
           if (text_msg && config.LOGS)
             console.log(
               `At : ${
@@ -141,10 +149,7 @@ async function Xasena() {
               return;
             let comman;
             if (text_msg) {
-              comman = text_msg
-                ? text_msg[0] +
-                  text_msg.slice(1).trim().split(" ")[0].toLowerCase()
-                : "";
+              comman = text_msg.trim().split(/ +/)[0];
               msg.prefix = new RegExp(config.HANDLERS).test(text_msg)
                 ? text_msg.split("").shift()
                 : ",";
@@ -180,24 +185,17 @@ async function Xasena() {
         console.log(e + "\n\n\n\n\n" + JSON.stringify(msg));
       }
     }
-    if (connection === "close") {
-      console.log(s);
-      console.log(
-        "Connection closed with bot. Please put New Session ID again."
-      );
-      Xasena().catch((err) => console.log(err));
-    } else {
-      /*
-       */
-    }
   });
-  process.on("uncaughtException", async (err) => {
+  process.on("uncaughtException", (err) => {
     let error = err.message;
-    await conn.sendMessage(conn.user.id, { text: error });
+    // conn.sendMessage(conn.user.id, { text: error });
     console.log(err);
   });
 }
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => console.log(`Inrl Server listening on port http://localhost:${port}`));
 setTimeout(() => {
-  Xasena().catch((err) => console.log(err));
-}, 3000);
+  ToxicLeo();
+}, 7000);
